12ad2e13b641df9771ffc8286e4c6440211238d1
XLasagna99 <aengus_leman@yahoo.com.sg>
2026-02-09 23:48:20 +0800

    Added Vector Store Ingestion Code and Placeholder Topic Detection Function

--- app/rag/ingestion.py
+++ app/rag/ingestion.py
@@ -1,22 +1,14 @@
+import os
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
 
-# Read in State of the Union Address File
-with open("2024_state_of_the_union.txt") as f:
-    state_of_the_union = f.read()
-
-# Initialize Text Splitter
-text_splitter = CharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200,
-    length_function=len
-)
-
-# Create Documents (Chunks) From File
-texts = text_splitter.create_documents([state_of_the_union])
-
+# load in the .env variables
+load_dotenv()
 
 def rag_document_ingestion(textfile):
-    """Ingest documents for RAG pipeline."""
+    """Chunk documents from text file for RAG ingestion."""
     with open(textfile) as f:
         document = f.read()
     
@@ -30,4 +22,27 @@ def rag_document_ingestion(textfile):
     # Create Documents (Chunks) From File
     texts = text_splitter.create_documents([document])
     return texts
-    
+
+
+def vector_store_name_detection(topic):
+    """Generate Vector Store name based on topic."""
+    vector_store_name = topic.lower().replace(" ", "_") + "_knowledge_base"
+    return vector_store_name
+
+
+def vector_store_ingestion(texts, vector_store_name):
+    """Ingest documents into Vector Store."""
+    # Initialize OpenAI Embeddings
+    embeddings = OpenAIEmbeddings(
+        model=os.getenv("OPENAI_EMBEDDING_MODEL")
+    )
+
+    # Initialize ChromaDB as Vector Store
+    vector_store = Chroma(
+        collection_name=vector_store_name,
+        embedding_function=embeddings
+    )
+
+    # Add Documents to Vector Store
+    vector_store.add_documents(texts)
+    return vector_store
--- app/rag/vector_store.py
+++ app/rag/vector_store.py
@@ -1,9 +1,17 @@
-from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
+# imports
+import os
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
 
-def load_vector_store(persist_dir: str):
-    embeddings = OpenAIEmbeddings()
-    return Chroma(
-        persist_directory=persist_dir,
-        embedding_function=embeddings
-    )
+# load in the .env variables
+load_dotenv()
+
+# Get Embeddings Model
+embeddings = OpenAIEmbeddings(model=os.getenv("TEXT_EMBEDDING_MODEL"))
+
+# Initialize ChromaDB as Vector Store
+vector_store = Chroma(
+    collection_name="football_knowledge_base",
+    embedding_function=embeddings
+)
